67f39008dc21bcccd1321ac98d0fc7a8916f439b
Jonathan Bergen <34497449+JonathanBergen@users.noreply.github.com>
2023-03-06 09:00:53 -0500

    wrote out the reduction kernel

--- lab4-reduction-and-prefix-scan/4.1-reduction/kernel.cu
+++ lab4-reduction-and-prefix-scan/4.1-reduction/kernel.cu
@@ -18,4 +18,29 @@ __global__ void reduction(float *out, float *in, unsigned size)
     ********************************************************************/
 
     // INSERT KERNEL CODE HERE
+
+    // Shared memory
+    __shared__ int sData[];
+
+    // Store the thread id, in both local and global context
+    unsigned int thrId = threadId.x;
+    unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+    // Load each thread's data
+    sData[thrId] = in[i];
+    __syncthreads();
+
+    // Stride through the elements
+    for (unsigned int stride = 2; stride < blockDim.x; stride *= 2) {
+        if (thrId % stride == 0) {
+            sData[thrId] += sData[thrId + stride / 2];
+        }
+        __syncthreads();
+    }
+
+    // Write the partial sum back to global memory
+    if (thrId == 0) {
+        out[blockIdx.x] = sData[0];
+    }
+
 }
