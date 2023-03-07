aaca7dda7373851f93491749281cc52e0fe125cc
Jonathan Bergen <34497449+JonathanBergen@users.noreply.github.com>
2023-03-07 11:08:35 -0500

    started on the scan algorithm

--- lab4-reduction-and-prefix-scan/4.2-prefix-scan/kernel.cu
+++ lab4-reduction-and-prefix-scan/4.2-prefix-scan/kernel.cu
@@ -21,6 +21,24 @@ GPU memory if you need to
 void preScan(float *out, float *in, unsigned in_size)
 {
     // INSERT CODE HERE
-	
-}
+    extern __shared__ float temp[];
+    int thrId = threadIdx.x;
+    int offset = 1;
 
+    // Load input
+    temp[thrId * 2] = in[thrId * 2];
+	temp[thrId * 2 + 1] = in[thrId * 2 + 1];
+    
+    // The downsweep, which builds the sums in place.
+    for (int d = in_size >> 1; d > 0; d >>= 1) {
+        // d is the floored half of array size
+        // d is divided by 2 with every iteration, ending when it rounds to 0
+        __syncthreads();
+        
+        if (thrId < d) {
+           int ai = offset * (thrId * 2 + 1) - 1;
+           int bi = offset * (thrId * 2 + 2) - 1;
+
+        }
+    }
+}
